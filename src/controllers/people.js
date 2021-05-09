ff098986920a191a36329e432839ca351f2e9c38
teapot <kipronofb@gmail.com>
2021-05-09 23:04:56 +0300

    update: validate data

--- src/controllers/people.js
+++ src/controllers/people.js
@@ -14,6 +14,10 @@ export function findPeople  (request, response) {
 
 export function addPerson  (request, response) {
     const {name, age} = request.body
+    if(!name | !age){
+        return response.status(400)
+        .send({status: 'error', data: {error: 'name and age required'}})
+    }
     postPeople([{name, age}]).then(people=>{
         // console.log(people)
         return response.status(200)
